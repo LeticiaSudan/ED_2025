6e92724a47e9d870ba594e984b8ef88d93e425aa
Aluno <aluno@IPIRANGA.FATEC>
2025-04-11 18:18:31 -0300

    learn: recursao

--- aulaED/conteudo_aulas/recursao.c
+++ aulaED/conteudo_aulas/recursao.c
@@ -2,8 +2,11 @@
 
 double fat_it (int n){
     double f = 1;
-    for(int i = 2; i <= n; i++){
-        f = f * i;
+    // for(int i = 2; i <= n; i++){
+    //     f = f * i;
+    // }
+    while(n > 1){
+        f = f * n--;
     }
     return f;
 }
@@ -18,6 +21,26 @@ long fibonacci (int n) {
     return fibonacci(n-1) + fibonacci(n-2);
 }
 
+void exibe_1_a_n_rec1 (int i, int n) {
+    printf("%d ", i);
+    if(i < n) exibe_1_a_n_rec1(i+1, n);
+}
+
+void exibe_1_a_n_rec2 (int i, int n) {
+    if(i < n) exibe_1_a_n_rec2(i+1, n);
+    printf("%d ", i);
+}
+
+void exibe_1_a_n_rec3 (int n) {
+    printf("%d ", n);
+    if(n > 1) exibe_1_a_n_rec3(n - 1);
+}
+
+void exibe_1_a_n_rec4 (int n) {
+    if(n > 1) exibe_1_a_n_rec4(n - 1);
+    printf("%d ", n);
+}
+
 int main (){
     for(int i = 0; i <= 20; i++){
         printf("fatorial de %d: %.0ld\n", i, fat_it(i));
@@ -27,7 +50,15 @@ int main (){
         printf("fatorial de %d: %.0ld\n", i, fat_rec(i));
     }
 
-    for(int i = 0; i <=46; i++){
+    for(int i = 0; i <=15; i++){
         printf("fibonacci em %d = %.0ld\n", i, fibonacci(i));
     }
+    exibe_1_a_n_rec1(1, 5);
+    printf("\n");
+    exibe_1_a_n_rec2(1, 5);
+    printf("\n");
+    exibe_1_a_n_rec3(5);
+    printf("\n");
+    exibe_1_a_n_rec4(5);
+    return 0;
 }
